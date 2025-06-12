69c8c39d79041758e8e5c3faa868ad56f0d6ad17
Ricardo A Sanjur G <ricardosanjurg@gmail.com>
2025-06-12 16:10:27 -0500

    hicimos el relog de js

---
+++ src/02 - css + js clock/script.js
@@ -0,0 +1,20 @@
+const secondhand = document.querySelector('.second-hand');
+const minutehand = document.querySelector('.min-hand');
+const hourhand = document.querySelector('.hour-hand');
+
+function setDate(){
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondhand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minutehand.style.transform = `rotate(${minutesDegrees}deg)`;
+  
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hourhand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
